34c49f18a201e58e57bae2a48f7c62e3f16efdc9
Richard <richard.wigley@gmail.com>
2021-05-30 19:59:34 +0100

    Rails dokku checks file added
      http://dokku.viewdocs.io/dokku/deployment/zero-downtime-deploys/#customizing-checks

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/check.txt', to: proc {[200, {}, ['simple_check']]}
 end
